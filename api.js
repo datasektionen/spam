8dc1a53f9dd99791f43d664f04e0d2724f3450d9
Andreas Mårtensson <addem1234@gmail.com>
2017-09-29 00:31:38 +0200

    Templating?

--- api.js
+++ api.js
@@ -1,72 +1,94 @@
-var express = require('express');
-var router  = express.Router();
-var nodemailer = require('nodemailer');
-var ses = require('nodemailer-ses-transport');
-var fetch = require('node-fetch');
+const router  = require('express').Router();
 
-var transporter = nodemailer.createTransport(ses({
+const fetch = require('node-fetch');
+
+const nodemailer = require('nodemailer');
+const ses = require('nodemailer-ses-transport');
+const Email = require('email-templates');
+
+const md = require('markdown-it')({
+  html: true,
+  linkify: true
+});
+
+
+const transporter = nodemailer.createTransport(ses({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID, //AWS key id
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, //AWS secret
     region: 'eu-west-1'
 }));
 
 //Emails that been verified that we can send from
-var verifiedFromEmails = ['valberedning@d.kth.se'];
+const verifiedFromEmails = ['valberedning@d.kth.se'];
 
-var errorMessage = function(res, message) {
+const errorMessage = (res, message) => {
   res.status(400);
   res.send(message);
 }
 
-var sendMail = function(req, res) {
+const sendMail = (req, res) => {
     //Do some error checking
     if (!req.body.to) return errorMessage(res, 'Missing field: to');
-    //We only allow to send from verified email addresses or anything ending with @datasektionen.se.
     if (!req.body.from) return errorMessage(res, 'Missing field: from');
-    //TODO: there is some error here, seems to let you send as any email currently.
-    if (verifiedFromEmails.indexOf(req.body.from) < 0 && !req.body.from.endsWith('@datasektionen.se')) {
+    if (!req.body.subject) return errorMessage(res, 'Missing field: subject');
+    if (!req.body.content)
+      return errorMessage(res, 'Missing field: content');
+    
+    //We only allow to send from verified email addresses or anything ending with @datasektionen.se.
+    const isVerified = verifiedFromEmails.includes(req.body.from);
+    const isDatasektionen = req.body.from.endsWith('@datasektionen.se');
+    if (!(isVerified || isDatasektionen)) {
         return errorMessage(res, 'Invalid from address: ' + req.body.from);
     }
-    if (!req.body.subject) return errorMessage(res, 'Missing field: subject');
-    if (!req.body.html) return errorMessage(req, 'Missing field: html');
+
+    const template = req.body.template;
+    //TODO: Check that the template exists!
+    if(template && false)
+      return errorMessage(res, 'Invalid template: ' + template);
 
     //Optional replyTo field, either the same as from or anything you want.
-    var replyTo = req.body.replyTo || req.body.from;
+    const replyTo = req.body.replyTo || req.body.from;
 
-    var email = {
+    const email = new Email({
+      htmlToText: false,
+      transport: transporter,
+      views: {
+        options: { extension: 'ejs' }
+      }
+    });
+
+    email.send({
+      message: {
         from: req.body.from, // sender address
         replyTo: replyTo, // Not needed unless this address will be different from the above.
+        subject: req.body.subject, // Subject has to be templated?
         to: req.body.to, // list of receivers
-        subject: req.body.subject, // Subject line
-        html: req.body.html // html body
-    };
-
-    transporter.sendMail(email, function(err, info){
-        if (err){
-          console.log(err);
-          res.status(400);
-          res.send('amazon email message error');
-        } else {
-          console.log('Sent email, messageid: ' + info.messageId);
-          res.status(200);
-          //We are lazy lets just send the info.
-          res.send(info);
-        }
+      },
+      template: template || 'default',
+      locals: {
+        content: md.render(req.body.content),
+        raw_content: req.body.content
+      }
+    }).then(status => {
+      console.log('status', status);
+      res.status(200);
+      res.send(status)
+    }).catch(error => {
+      console.log('error', error);
+      res.status(500);
+      res.send(error);
     });
 };
 
 router.post('/sendmail', function(req, res) {
     //check api key such that they are actually allowed to send email
     fetch('https://pls.datasektionen.se/api/token/' + req.body.key + '/spam')
-    .then(function(json) { return json.json()})
-    .then(function(json) {
-        if(!json.includes('send')) return errormessage(res, 'bad api key');
+    .then(response => response.json())
+    .then(json => {
+        if(!json.includes('send')) return errorMessage(res, 'Bad api key');
         //otherwise just send the mail.
-        console.log(json)
         sendMail(req, res);
-    }).catch(function(err) {
-      res.send(err);
-    });
+    }).catch(err => res.send(err));
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var api = require('./api');
+const express = require('express');
+const bodyParser = require('body-parser');
+const api = require('./api');
 
-var app = express();
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api', api);
 
-var port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, function() {
 	console.log('Express server listening on port ' + port);
